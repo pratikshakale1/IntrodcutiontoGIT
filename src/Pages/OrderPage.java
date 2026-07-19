d7713938ba8340e30bae322fb751f4541dd14e3e
pratiksha <pratikshakale179@gmail.com>
2026-07-19 16:28:26 +0530

    added order page

---
+++ src/Pages/OrderPage.java
@@ -0,0 +1,15 @@
+package Pages;
+
+public class OrderPage {
+	
+	public void gt() {
+		System.out.println("Order Page");
+	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub	
+		
+	}
+
+}
