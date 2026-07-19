5d19a7d0e977329bc837bd3045bc3a3225ccf910
pratiksha <pratikshakale179@gmail.com>
2026-07-19 16:57:20 +0530

    added order page

---
+++ src/Pages/Orderpage.java
@@ -0,0 +1,10 @@
+package Pages;
+
+public class Orderpage {
+
+	
+	public void order()
+{
+System.out.println("order page");		
+}
+}
---
+++ src/Test/OrderTestPage.java
@@ -0,0 +1,9 @@
+package Test;
+
+public class OrderTestPage {
+	
+	public void orderTest() {
+		
+	}
+
+}
