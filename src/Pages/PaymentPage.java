9b0d4a50e38f3ebcef059b5d435bb5d5cac071b6
pratiksha <pratikshakale179@gmail.com>
2026-07-19 20:54:08 +0530

    paymentpage added

---
+++ src/Pages/PaymentPage.java
@@ -0,0 +1,14 @@
+package Pages;
+
+public class PaymentPage {
+	
+	public void payment()
+	{
+		System.out.println("payment");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
